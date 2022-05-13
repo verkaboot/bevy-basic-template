c0b71bd6b676f6ccdf586fe674c9baecf324c933
Verkaboot <verkaboot@gmail.com>
2022-05-12 20:29:30 -0600

    Update Bevy version

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use bevy::prelude::*;
-use bevy_editor_pls::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(EditorPlugin)
         .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
         .add_plugin(bevy::diagnostic::EntityCountDiagnosticsPlugin)
         .run();
